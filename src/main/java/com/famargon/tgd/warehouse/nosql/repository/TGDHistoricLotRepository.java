ab04a1c6082d6da369285f0f6e2c0931f0b3d4ba
famargon <famargon13@gmail.com>
2018-01-03 21:52:50 +0100

    hibernate.ddl-auto:none and historicLot to the api

--- src/main/java/com/famargon/tgd/warehouse/nosql/repository/TGDHistoricLotRepository.java
+++ src/main/java/com/famargon/tgd/warehouse/nosql/repository/TGDHistoricLotRepository.java
@@ -1,9 +1,11 @@
 package com.famargon.tgd.warehouse.nosql.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.famargon.tgd.warehouse.nosql.datamodel.TGDHistoricLot;
 
+@RepositoryRestResource(path="historiclot")
 public interface TGDHistoricLotRepository extends CrudRepository<TGDHistoricLot, String> {
 
 }
